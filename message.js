47d03fd0ff06be53546bada822a0052a52cee092
thanuja-22 <tanujakollipara9849@gmail.com>
2020-09-02 20:51:13 +0530

    first commit

---
+++ message.js
@@ -0,0 +1,16 @@
+// import MessageMixer from './maink'
+// const MessageMixer=require('./maink.js');
+import {countCharacter,capitalizeFirstCharacterOfWords,reverseWord,reverseAllWords,replaceFirstOccurence,replaceAllOccurrences,
+  encode,palindrome,pigLatin} from './maink';
+function displayMessage() {
+  console.log(countCharacter("What is the color of the sky?", "t"));
+  console.log(capitalizeFirstCharacterOfWords("What is the color of the sky?"));
+  console.log(reverseWord("What is the color of the sky?"));
+  console.log(reverseAllWords("What is the color of the sky?"));
+  console.log(replaceFirstOccurence("What is the color of the sky?", "sky", "water"));
+  console.log(encode("What is the color of the sky?"));
+  console.log(palindrome("cheese"));
+  console.log(pigLatin("What is the color of the sky",'-'));
+}
+
+displayMessage();
